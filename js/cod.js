8f548868ae58bafc2ab508519bc33fd3925dd051
Jean Marcos da Rosa <jeanmarcosdarosa@gmail.com>
2018-11-14 18:11:01 -0200

    funciona mi fadeOut

--- js/cod.js
+++ js/cod.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
+    $(window).load(function() {
+        fadeIn()
+    });
     $( window ).scroll(function() {
         if ($(this).scrollTop()) {
             $(".menu").addClass("backgroun-scroll");
-            $(".nav").addClass("nav2");
+            $(".nav").addClass("nav2");  
+            //$('#container').css('opacity', 1);          
 		}else{
             $(".menu").removeClass("backgroun-scroll"); 
-            $(".nav").removeClass("nav2");                                          
+            $(".nav").removeClass("nav2");    
+            //$('#container').css('opacity', 0);                                                 
 		}          
     });
+    
+    if ($('#container').css('opacity') == 0) {
+        $('#container').css('opacity', 1);
+    }
+    /*else {
+        $('#container').css('opacity', 0);
+    } */
 });
-
-
